6b8370fefc1f44d40089ec89e862c76724456036
Carlos Aguirre <calejandroaguirre@outlook.com>
2017-10-26 16:41:58 -0600

    Added iterator pattern

---
+++ DesignPatterns/Patterns/Iterator/DessignPatterns.Patterns.Iterator/Aggregate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DessignPatterns.Patterns.Iterator
+{
+    /// <summary>
+    /// The 'Aggregate' abstract class
+    /// </summary>
+    abstract class Aggregate
+    {
+        public abstract Iterator CreateIterator();
+    }
+}
---
+++ DesignPatterns/Patterns/Iterator/DessignPatterns.Patterns.Iterator/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DessignPatterns.Patterns.Iterator
+{
+    public class Client
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            ConcreteAggregate a = new ConcreteAggregate();
+            a[0] = "Item A";
+            a[1] = "Item B";
+            a[2] = "Item C";
+            a[3] = "Item D";
+
+            // Create Iterator and provide aggregate
+            Iterator i = a.CreateIterator();
+
+            Console.WriteLine("Iterating over collection:");
+
+            object item = i.First();
+            while (item != null)
+            {
+                Console.WriteLine(item);
+                item = i.Next();
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
