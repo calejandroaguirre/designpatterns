aae8142be057c8a46cc302c2db962a9fcec18ef5
Carlos Aguirre <calejandroaguirre@outlook.com>
2017-10-26 18:22:55 -0600

    Added mediator pattern

---
+++ DesignPatterns/Patterns/Mediator/DesignPatterns.Patterns.Mediator/Colleague.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns.Mediator
+{
+    /// <summary>
+    /// The 'Colleague' abstract class
+    /// </summary>
+    abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        // Constructor
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+}
