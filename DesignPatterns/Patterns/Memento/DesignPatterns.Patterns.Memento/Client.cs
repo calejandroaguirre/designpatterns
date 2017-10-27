f693a3e40b5152b89567bc165ee3b26f91d9b0c7
Carlos Aguirre <calejandroaguirre@outlook.com>
2017-10-26 18:33:54 -0600

    Added memento pattern

---
+++ DesignPatterns/Patterns/Memento/DesignPatterns.Patterns.Memento/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns.Memento
+{
+    public class Client
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            Originator o = new Originator
+            {
+                State = "On"
+            };
+
+            // Store internal state
+            Caretaker c = new Caretaker
+            {
+                Memento = o.CreateMemento()
+            };
+
+            // Continue changing originator
+            o.State = "Off";
+
+            // Restore saved state
+            o.SetMemento(c.Memento);
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
