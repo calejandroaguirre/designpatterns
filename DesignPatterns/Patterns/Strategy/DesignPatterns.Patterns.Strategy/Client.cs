730883307198d828885d9bfa673deff1dd1ac832
Carlos Aguirre <calejandroaguirre@outlook.com>
2017-10-31 23:51:17 -0700

    Added Strategy Pattern

---
+++ DesignPatterns/Patterns/Strategy/DesignPatterns.Patterns.Strategy/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns.Strategy
+{
+    public class Client
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            // Two contexts following different strategies
+            SortedList studentRecords = new SortedList();
+
+            studentRecords.Add("Samual");
+            studentRecords.Add("Jimmy");
+            studentRecords.Add("Sandra");
+            studentRecords.Add("Vivek");
+            studentRecords.Add("Anna");
+
+            studentRecords.SetSortStrategy(new QuickSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new ShellSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new MergeSort());
+            studentRecords.Sort();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
