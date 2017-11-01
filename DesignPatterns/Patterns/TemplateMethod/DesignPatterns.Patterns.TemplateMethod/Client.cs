c8f178d62177f282e3a15b8d30d1b67c3994ce35
Carlos Aguirre <calejandroaguirre@outlook.com>
2017-10-31 23:44:45 -0700

    Added TemplateMethod pattern

---
+++ DesignPatterns/Patterns/TemplateMethod/DesignPatterns.Patterns.TemplateMethod/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns.TemplateMethod
+{
+    public class Client
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            DataAccessObject daoCategories = new Categories();
+            daoCategories.Run();
+
+            DataAccessObject daoProducts = new Products();
+            daoProducts.Run();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
